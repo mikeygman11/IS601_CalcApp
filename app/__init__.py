fc561340e8ff35b01d3094e567a7512628496190
mikeygman11 <mgalanaugh15@gmail.com>
2025-02-26 00:53:39 -0500

    Adding plugins

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,28 @@
 from app.commands import CommandHandler
-from app.commands.add import AddCommand
-from app.commands.subtract import SubtractCommand
-from app.commands.multiply import MultiplyCommand
-from app.commands.divide import DivideCommand
+import pkgutil
+import importlib
+from app.commands import Command
+
 import sys
 class App:
     def __init__(self):  # Constructor
         self.command_handler = CommandHandler()
 
+    def load_plugins(self):
+        # Dynamically load all plugins in the plugins directory
+        plugins_package = 'app.plugins'
+        for _, plugin_name, is_pkg in pkgutil.iter_modules([plugins_package.replace('.', '/')]):
+            if is_pkg:  # Ensure it's a package
+                plugin_module = importlib.import_module(f'{plugins_package}.{plugin_name}')
+                for item_name in dir(plugin_module):
+                    item = getattr(plugin_module, item_name)
+                    try:
+                        if issubclass(item, (Command)):  # Assuming a BaseCommand class exists
+                            self.command_handler.register_command(plugin_name, item())
+                    except TypeError:
+                        continue  # If item is not a class or unrelated class, just ignore
     def start(self):
-        # Register command classes, NOT instances
-        self.command_handler.register_command("Add", AddCommand)
-        self.command_handler.register_command("Subtract", SubtractCommand)
-        self.command_handler.register_command("Divide", DivideCommand)
-        self.command_handler.register_command("Multiply", MultiplyCommand)
-
+        self.load_plugins()
         print("Type 'exit' to exit.")
         while True:  # REPL (Read, Evaluate, Print, Loop)
             command_name = input(">>> ").strip()
---
+++ app/plugins/subtract/__init__.py
@@ -0,0 +1,10 @@
+from app.commands import Command
+#creating the Subtract class
+
+class SubtractCommand(Command):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def execute(self):
+        return self.x - self.y
