e3fef4ff6ed16c0eb3b9d72625d8c2beed1a863b
mikeygman11 <mgalanaugh15@gmail.com>
2025-02-25 23:32:50 -0500

    Refactoring test functions

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,16 +1,30 @@
 import pytest
 from app import App
-from app.commands.goodbye import GoodbyeCommand
-from app.commands.greet import GreetCommand
+from app.commands.add import AddCommand
+from app.commands.subract import SubtractCommand
+from app.commands.divide import DivideCommand
+from app.commands.multiply import MultiplyCommand
 
-def test_greet_command(capfd):
-    command = GreetCommand()
+def test_add_command(capfd):
+    command = AddCommand()
     command.execute()
     out, err = capfd.readouterr()
-    assert out == "Hello, World!\n", "The GreetCommand should print 'Hello, World!'"
+    assert out == "Hello, World!\n", "The Add Command should print 'Adding!'"
 
-def test_goodbye_command(capfd):
-    command = GoodbyeCommand()
+def test_subtract_command(capfd):
+    command = SubtractCommand()
     command.execute()
     out, err = capfd.readouterr()
-    assert out == "Goodbye\n", "The GreetCommand should print 'Hello, World!'"
+    assert out == "Hello, World!\n", "The Subtract Command should print 'Subtract!'"
+
+def test_divide_command(capfd):
+    command = DivideCommand()
+    command.execute()
+    out, err = capfd.readouterr()
+    assert out == "Hello, World!\n", "The Divide Command should print 'Divide!'"
+
+def test_multiply_command(capfd):
+    command = MultiplyCommand()
+    command.execute()
+    out, err = capfd.readouterr()
+    assert out == "Hello, World!\n", "The Multiply Command should print 'Mult!'"
